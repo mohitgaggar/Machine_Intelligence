18a755c8bb80178c1976dea8a24066d2f9b58b3f
Rav317 <raghavb144@gmail.com>
2020-08-30 14:01:56 +0530

    Updated Files

--- Assignment1/Assignment1.py
+++ Assignment1/Assignment1.py
@@ -9,31 +9,26 @@ import random
 	#input:pandas_dataframe
 	#output:int/float/double/large
 
-# function to return the log based 2 of a number ,workaround for log(0) is returning 0
+def get_entropy(p, n):
+	
+	ent = ((-p/(p+n)*np.log2(p/(p+n))) if p != 0 else 0) \
+			- ((n/(n+p)*np.log2(n/(n+p))) if n != 0 else 0)
 
-def log_2(number):
-	if(number==0):
-		return 0
-	else:
-		number=np.array([number])
-		return np.log2(number)
-
+	return ent
 
 def get_entropy_of_dataset(df):
-	entropy = 0
-	num_columns=len(df.columns)
-	num_rows=len(df)
-	output=df["play"]
-	p=0
-	n=0
-	for i in output:
-		if(i=="yes"):
+
+	p = 0
+	n = 0
+
+	for i in df.iloc[:, -1]:
+		if(i == "yes" or i == 1):
 			p+=1
 		else:
 			n+=1
-	p_ratio=(p/(p+n))
-	n_ratio=1-p_ratio
-	entropy=-1*((p_ratio)*log_2(p_ratio)+(n_ratio)*log_2(n_ratio))
+
+	entropy = get_entropy(p,n)
+
 	return entropy
 
 
@@ -41,33 +36,30 @@ def get_entropy_of_dataset(df):
 '''Return entropy of the attribute provided as parameter'''
 	#input:pandas_dataframe,str   {i.e the column name ,ex: Temperature in the Play tennis dataset}
 	#output:int/float/double/large
-
-
 def get_entropy_of_attribute(df,attribute):
-	entropy_of_attribute = 0
-	col_=df[attribute]
-	output=df["play"]
-	num_rows=len(df)
-	unique_vals_in_col=list(set(col_))
-	p={}
-	n={}
-
-	for i in unique_vals_in_col:
-		p[i]=0
-		n[i]=0
 	
+	num_rows = len(df)
+	vals = {}
 	for i in range(num_rows):
-		if(output[i]=="yes"): 
-			p[col_[i]]+=1 
+		if(df[attribute][i] not in vals):
+			vals[df[attribute][i]] = [0,0]
+			if(df.iloc[:, -1][i] == "yes" or df.iloc[:, -1][i] == 1):
+				vals[df[attribute][i]][0]+=1
+			else:
+				vals[df[attribute][i]][1]+=1
 		else:
-			n[col_[i]]+=1
-	
-	for i in unique_vals_in_col:
-		p_ratio=(p[i]/(p[i]+n[i]))
-		n_ratio=1-p_ratio
-		entropy=-1*((p_ratio)*log_2(p_ratio)+(n_ratio)*log_2(n_ratio))
-		entropy_of_attribute+=((p[i]+n[i])/(num_rows))*entropy
-	
+			if(df.iloc[:, -1][i] == "yes" or df.iloc[:, -1][i] == 1):
+				vals[df[attribute][i]][0]+=1
+			else:
+				vals[df[attribute][i]][1]+=1
+
+	entropy_of_attribute = 0
+
+	for val in vals:
+		p = vals[val][0]
+		n = vals[val][1]
+		entropy_of_attribute += ((p+n) * get_entropy(p,n)) / num_rows
+
 	return abs(entropy_of_attribute)
 
 
@@ -76,8 +68,9 @@ def get_entropy_of_attribute(df,attribute):
 	#input:int/float/double/large,int/float/double/large
 	#output:int/float/double/large
 def get_information_gain(df,attribute):
-	information_gain = 0
-	information_gain = get_entropy_of_dataset(df)-get_entropy_of_attribute(df,attribute)
+	
+	information_gain = get_entropy_of_dataset(df) - get_entropy_of_attribute(df, attribute)
+
 	return information_gain
 
 
@@ -86,24 +79,25 @@ def get_information_gain(df,attribute):
 	#input: pandas_dataframe
 	#output: ({dict},'str')     
 def get_selected_attribute(df):
-	attributes=list(df.columns)
-	attributes.remove('play') 
+   
 	information_gains={}
-	maxx=0
-	
-	for attribute in attributes:
-		information_gains[attribute]=get_information_gain(df,attribute)
-		if(information_gains[attribute] > maxx):
-			selected_column=attribute
-			maxx=information_gains[attribute]	
+	selected_column=''
 
+	for column in df.iloc[:,:-1].columns:
+		information_gains[column] = get_information_gain(df, column)
+
+
+	maxInfGain = 0
+	for col in information_gains:
+		if(information_gains[col] > maxInfGain):
+			maxInfGain = information_gains[col]
+			selected_column = col
 	'''
 	Return a tuple with the first element as a dictionary which has IG of all columns 
 	and the second element as a string with the name of the column selected
 
 	example : ({'A':0.123,'B':0.768,'C':1.23} , 'C')
 	'''
-	# print(information_gains,selected_column)
 
 	return (information_gains,selected_column)
 
